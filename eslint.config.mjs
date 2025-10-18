a09d7663575963967d7b771603dbe8999514dfbc
Leonardo Santino <79323700+leonardosantino@users.noreply.github.com>
2025-10-18 11:53:06 -0300

    chore: update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,19 @@
 import { FlatCompat } from "@eslint/eslintrc";
 
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-});
+const compat = new FlatCompat({});
 
 const eslintConfig = [
-  ...compat.config({
-    extends: ["next/core-web-vitals", "next/typescript"],
-  }),
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+      "coverage/**",
+    ],
+  },
 ];
 
 export default eslintConfig;
