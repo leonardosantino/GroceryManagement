496241e6bbbdce22968c60ab28a31421b9979699
Leonardo Santino <79323700+leonardosantino@users.noreply.github.com>
2025-11-26 17:18:40 -0300

    fix: next images

--- Docker.sh
+++ Docker.sh
@@ -3,4 +3,4 @@
 set -e
 
 npx next build
-docker build --platform linux/amd64 -t leonardosantino/grocerymanagement:0.0.1 .
+docker build --platform linux/amd64 -t leonardosantino/grocerymanagement:0.0.12 .
